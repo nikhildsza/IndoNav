c49595bd22b100a1108b452c72aa6a4763b8adad
nikhildsza <nikhilm.dsouza@gmail.com>
2024-12-16 21:56:21 +0530

    Source code added

---
+++ APKs/v1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void setNavigationTarget::Start()
+extern void setNavigationTarget_Start_mC9C3879ECC50A3459EBAF8510366EF7AA36EBD13 (void);
+// 0x00000002 System.Void setNavigationTarget::Update()
+extern void setNavigationTarget_Update_m5431AD9523FCDAA8B01AB0CEC56E7FAAF9E61EAB (void);
+// 0x00000003 System.Void setNavigationTarget::.ctor()
+extern void setNavigationTarget__ctor_m2BE8DECB903869F2B00C935BDAB4D8AB0F5A1795 (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	setNavigationTarget_Start_mC9C3879ECC50A3459EBAF8510366EF7AA36EBD13,
+	setNavigationTarget_Update_m5431AD9523FCDAA8B01AB0CEC56E7FAAF9E61EAB,
+	setNavigationTarget__ctor_m2BE8DECB903869F2B00C935BDAB4D8AB0F5A1795,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	4101,
+	4101,
+	4101,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
